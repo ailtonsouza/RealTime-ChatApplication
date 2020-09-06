d6edfa7420641c9f973f53526ad1563b7ae1c944
Ailton Souza <ailton1234@hotmail.com>
2020-09-06 15:26:13 -0300

    Uploado do projeto Real time chat application com socket.io

---
+++ src/index.js
@@ -0,0 +1,44 @@
+const path = require('path');
+const http = require('http');
+const express = require('express');
+const socketio = require('socket.io');
+const Filter = require('bad-words');
+const { generateMessage, generateLocationMessage } = require('./utils/messages.js');
+const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
+
+const port = process.env.PORT || 3000;
+const publicDirectoryPath = path.join(__dirname, '../public');
+
+app.use(express.static(publicDirectoryPath));
+
+io.on('connection', (socket) => {
+
+  socket.emit('message', generateMessage('Wellcome!'));
+
+  socket.broadcast.emit('message', generateMessage('A new user has joined!'));
+
+  socket.on('sendMessage', (message, callback) => {
+    const filter = new Filter();
+
+    if (filter.isProfane(message)) {
+      return callback('Profanity is not allowed!');
+    }
+    io.emit('message', generateMessage(message))
+    callback()
+  });
+
+  socket.on('sendLocation', (location, callback) => {
+    io.emit('locationMessage', generateLocationMessage(`https://google.com/maps?q=${location.latitude},${location.longitude}`));
+    callback()
+  });
+
+  socket.on('disconnect', () => {
+    io.emit('message', generateMessage('A user disconnected'))
+  })
+})
+
+server.listen(port, () => {
+  console.log(`Server is up on port ${port}!`)
+})
